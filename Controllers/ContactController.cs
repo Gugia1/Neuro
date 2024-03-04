7f8b80f8665731deb4035d8be4e12e87f6e69dce
TornikeTsereteli <tornike.tsereteli2003@gmail.com>
2024-03-04 13:42:35 +0400

    added endpoints: About/MethodName etc. you can found it in controller folder, relevant view is in View folder relevant folder name and then relevant .cshtml file. for example localhost:1234/About/Services is in. View->About->Services.html

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace neurobalance.com.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class ContactController:Controller
+{
+    [HttpGet("/Contact")]
+    public IActionResult Contacts()
+    {
+        return View();
+    }
+}
